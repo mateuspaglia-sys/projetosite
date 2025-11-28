71457e911b1112863eafe46755390795d2906770
Mateus Pagliarini <mateuspaglia@gmail.com>
2025-11-28 13:57:56 -0300

    versao final pronta

--- avaliacoes.js
+++ avaliacoes.js
@@ -1,2 +1,11 @@
-let stars = document.querySelectorAll('.star-icon'); document.addEventListener('click', function (e) { let classStar = e.target.classList; if (!classStar.contains('ativo')) { stars.forEach(function (star) { star.classList.remove('ativo'); }); classStar.add('ativo'); console.log(e.target.getAttribute('data-estrelinha')); } });
+let stars = document.querySelectorAll('.star-icon');
+ document.addEventListener('click', function (e) { 
+    let classStar = e.target.classList;
+     if (!classStar.contains('ativo')) { stars.forEach(function (star) { 
+        star.classList.remove('ativo');
+     });
+      classStar.add('ativo');
+       console.log(e.target.getAttribute('data-estrelinha'));
+     }
+           });
 
